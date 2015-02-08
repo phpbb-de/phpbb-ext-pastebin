54f532cdca91bce158139b93daecf89a3d8f96a4
Martin Beckmann <gn#36@phpbb.de>
2015-02-08 20:28:12 +0100

    Add presence check for Pastebin

--- tests/functional/install_test.php
+++ tests/functional/install_test.php
@@ -25,4 +25,10 @@ class install_test extends \phpbb_functional_test_case
 		$crawler = self::request('GET', 'viewtopic.php?f=1&t=1');
 		$this->assertContains('Welcome to phpBB3', $crawler->filter('h2')->text());
 	}
+
+	public function test_validate_pastebin()
+	{
+		$crawler = self::request('GET', 'app.php/pastebin/');
+		$this->assertContains('Pastebin', $crawler->filter('h2')->text());
+	}
 }
