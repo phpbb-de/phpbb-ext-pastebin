8c264d44ae1c8e5d5cf9be47c7b2609413d5dd08
Christian Schnegelberger <mail@crizzo.de>
2020-10-04 12:47:20 +0200

    New version 2.0.6

---
+++ migrations/v206.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *
+ * Pastebin extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2020 Crizzo <https://www.phpBB.de>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace phpbbde\pastebin\migrations;
+
+class v206 extends \phpbb\db\migration\migration
+{
+	static public function depends_on()
+	{
+		return array(
+			'\phpbbde\pastebin\migrations\v205',
+		);
+	}
+
+	public function update_data()
+	{
+		$data = array(
+			// Update version
+			array('config.update', array('pastebin_version', '2.0.6')),
+		);
+		return $data;
+	}
+}
