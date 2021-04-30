6f974847097c412a04bc86789e96a1b1b4051745
Christian Schnegelberger <mail@crizzo.de>
2021-04-30 11:29:41 +0200

    Run composer update no-dev

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInite29ac243941deb1509821f7b4d6204ed
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
